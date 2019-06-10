e6ba8b3e0f1c6223cecc1cbc88e63919c43b748b
Nick Wilburn <senior.crepe@gmail.com>
2019-06-10 10:52:01 -0700

    Fix lint failures

--- setup.py
+++ setup.py
@@ -1,4 +1,6 @@
 #! /usr/bin/env python
 import setuptools
-__name__ == '__main__' and setuptools.setup(use_scm_version=True, setup_requires=['setuptools-scm'])
 
+__name__ == "__main__" and setuptools.setup(
+    use_scm_version=True, setup_requires=["setuptools-scm"]
+)
